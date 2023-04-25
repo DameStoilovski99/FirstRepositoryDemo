aba816bbf99b7fbf1d8fa73990a765543cd3a1c3
dames <dame.stoilovski@gmail.com>
2023-04-25 21:46:01 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-//        ConcatenateTwoStrings concTwoStr = new ConcatenateTwoStrings();
-//        String firstWord = "Dame";
-//        String secondWord = "Dame";
-//        String result = concTwoStr.mergeTwoStrings(firstWord, secondWord);
-//        System.out.println(result);
+        ConcatenateTwoStrings concTwoStr = new ConcatenateTwoStrings();
+        String firstWord = "Dame";
+        String secondWord = "Dame";
+        String result = concTwoStr.mergeTwoStrings(firstWord, secondWord);
+        System.out.println(result);
 
-//        PalindromeCheck palindrome = new PalindromeCheck();
-//        String word = "121121";
-//        boolean result = palindrome.CheckForPalindrome(word);
-//        System.out.println(result);
+        PalindromeCheck palindrome = new PalindromeCheck();
+        String word = "121121";
+        boolean result = palindrome.CheckForPalindrome(word);
+        System.out.println(result);
 
         CheckTheSum sumChecker = new CheckTheSum();
         String first = "acb";
